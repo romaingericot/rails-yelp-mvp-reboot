c9f50dcd7b9ac1be4e5a538cf20bd120c3c10a13
Romain Gericot <romain.gericot@gmail.com>
2020-11-10 16:59:44 +0100

    Init controllers and routes for Restaurants and Reviews

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,6 +3,6 @@ class Restaurant < ApplicationRecord
 
   validates :name, :address, presence: true
   #validates :name, :address, :category, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, inclusion: { in: ['chinese', 'italian', 'japanese', 'french', 'belgian'] }
   #validates :category, acceptance: { accept: ["chinese", "italian", "japanese", "french", "belgian"] }
 end
