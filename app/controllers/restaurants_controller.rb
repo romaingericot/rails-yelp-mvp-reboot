4d2e92d21967e7453cc6b43d6d5322ea7ea795ac
Romain Gericot <romain.gericot@gmail.com>
2020-11-10 18:49:54 +0100

    Update Restaurants Controller with new and create methods + private method for restaurant_params + update views for new and show

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,5 +12,18 @@ class RestaurantsController < ApplicationController
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+      # render "new"
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
